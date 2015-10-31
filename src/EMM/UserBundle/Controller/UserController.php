39a2e78b0a657485a8f279f3d2dcf9fca87548fd
Alfieris <alfierisnoe@hotmail.com>
2015-10-31 17:34:37 +0000

    modificando mas

---
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace EMM\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        
+        $users = $em->getRepository('EMMUserBundle:User')->findAll();
+        
+        /* 
+        $res = 'Lista de usuarios: <br />';
+        
+        foreach($users as $user)
+        {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
+        }
+        
+        return new Response($res);
+        */
+        
+        return $this->render('EMMUserBundle:User:index.html.twig', array('users' => $users));
+    }
+    
+    public function viewAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+        
+        $user = $repository->find($id);
+        
+        // $user = $repository->findOneByUsername($nombre);
+        
+        return new Response('Usuario: ' . $user->getUsername() . ' con email: ' . $user->getEmail());
+    }
+    
+    
+     
+}
